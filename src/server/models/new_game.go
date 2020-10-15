2b8bb22f682c7798387af7997628a3c8b842b33f
Haseeb Majid <hello@haseebmajid.dev>
2020-10-15 10:52:07 +0100

    feat: implemented /game endpoints
    Implemented all of the endpoints for `/game`.

--- src/server/models/new_game.go
+++ src/server/models/new_game.go
@@ -1,6 +1,6 @@
 package models
 
 type NewGame struct {
-	Name     string `bson:"name" json:"name"`
-	RulesURL string `bson:"rules_url" json:"rules_url"`
+	Name     string `bson:"name" json:"name" validate:"required"`
+	RulesURL string `bson:"rules_url" json:"rules_url" validate:"required"`
 }
